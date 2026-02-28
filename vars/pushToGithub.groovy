c0a9f41119389ca9c8530e152fa80766872a15bd
thogue12 <hoguetimothy12@gmail.com>
2026-02-28 13:25:40 -0500

    another commit to the library

--- vars/pushToGithub.groovy
+++ vars/pushToGithub.groovy
@@ -14,16 +14,15 @@ def call(Map config =[:]){
                 git config user.email "thogue12@local.com"
                 git config user.name  "thogue12"
 
-                # 1. Fixed spelling to --porcelain
-                # 2. Used \\\$ so the shell handles the variable, not Groovy
-                if [ -n "\$(git status --porcelain ${filePath})" ]; then
+                # Use -uall to ensure git sees UNTRACKED files (newly created ones)
+                if [ -n "\$(git status --porcelain -uall ${filePath})" ]; then
                     echo "Changes detected in ${filePath}. Committing..."
                     git add ${filePath}
                     git commit -m "Automated update of ${filePath} from Jenkins build ${env.BUILD_NUMBER}"
                     git push https://\${GIT_USER}:\${GIT_PASS}@${repoUrl} HEAD:${branch}
                 else
                     echo "No changes detected on ${filePath}. Skipping git push"
-                fi 
+                fi
             """
         }
         return true
